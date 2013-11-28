cd3ecbd4db7a3f2f9d66d5a9afae1e800cb356e4
Simon Forman <forman.simon@gmail.com>
2013-11-28 15:26:59 -0800

    An "endpoint" for POSTing webhooks.

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-from flask import Flask, render_template
+import logging
+from flask import Flask, request, render_template, jsonify
+
+
+def _setup_log():
+  log = logging.getLogger('sosimple')
+  log.setLevel(logging.DEBUG)
+
+  sh = logging.StreamHandler()
+  sh.setLevel(logging.DEBUG)
+  sh.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s'))
+  log.addHandler(sh)
+
+  fh = logging.FileHandler('sosimple.log')
+  fh.setLevel(logging.INFO)
+  fh.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+  log.addHandler(fh)
+
+  return log
+
+
+log = _setup_log()
 
 
 app = Flask(__name__)
@@ -8,6 +29,13 @@ app.debug = True
 @app.route('/')
 def index():
   return render_template('index.html')
+
+
+@app.route('/hookey', methods=['POST'])
+def hookey():
+  data = dict(request.form)
+  log.info('data %r', data)
+  return jsonify(data=data)
 
 
 if __name__ == '__main__':
