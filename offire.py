9028fb3f842813317287c301863dfe9c88aa8b47
Simon Forman <forman.simon@gmail.com>
2013-11-30 20:57:39 -0800

    Just to ee it go again!

--- offire.py
+++ offire.py
@@ -4,5 +4,8 @@ class TestTest(unittest.TestCase):
   def test_der(self):
     self.assertEqual(23, 2 + 21)
 
+  def test_un(self):
+    self.assertEqual(23, 2 + 21)
+
 if __name__ == '__main__':
     unittest.main()
