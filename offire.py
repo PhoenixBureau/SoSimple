64bd8539ad361712e08994954551e5440044e1bc
Simon Forman <forman.simon@gmail.com>
2013-11-30 20:45:28 -0800

    Make a change and see if BB picks it up.

--- offire.py
+++ offire.py
@@ -1,8 +1,8 @@
 import unittest
 
 class TestTest(unittest.TestCase):
-  def test_shuffle(self):
-    print 'Yes!'
+  def test_der(self):
+    self.assertEqual(23, 2 + 21)
 
 if __name__ == '__main__':
     unittest.main()
