6f6134e1a6de6887f20421c70329baa1f22ad21a
Simon Forman <forman.simon@gmail.com>
2013-11-30 21:24:17 -0800

    simple test
    http://flask.pocoo.org/docs/testing/

--- offire.py
+++ offire.py
@@ -1,11 +1,22 @@
-import unittest
+import os, unittest, tempfile
+import main
+
 
 class TestTest(unittest.TestCase):
-  def test_der(self):
-    self.assertEqual(23, 2 + 21)
 
-  def test_un(self):
-    self.assertEqual(23, 2 + 21)
+  def setUp(self):
+    self.db_fd, main.app.config['DATABASE'] = tempfile.mkstemp()
+    main.app.config['TESTING'] = True
+    self.app = main.app.test_client()
+
+  def tearDown(self):
+    os.close(self.db_fd)
+    os.unlink(main.app.config['DATABASE'])
+
+  def test_hello(self):
+    rv = self.app.get('/')
+    self.assertTrue('Hello World!' in rv.data)
+
 
 if __name__ == '__main__':
     unittest.main()
